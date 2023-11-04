9b67543703a4b96f731e9ffdc985f9eff6711d33
frangoldammer <frangoldammer@gmail.com>
2023-11-04 15:41:07 -0300

    main drawer

--- app/src/main/java/com/example/tp3_grupo7_be/views/MainActivity.kt
+++ app/src/main/java/com/example/tp3_grupo7_be/views/MainActivity.kt
@@ -2,11 +2,60 @@ package com.example.tp3_grupo7_be.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.tp3_grupo7_be.R
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawer : DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        toggle = ActionBarDrawerToggle(this, drawer, R.string.nav_drawer_open, R.string.nav_drawer_close)
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        toggle.setDrawerIndicatorEnabled(false)
+        toggle.setHomeAsUpIndicator(getResources().getDrawable(R.drawable.drawer_icon))
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        toolbar.setNavigationOnClickListener {
+            if (drawer.isDrawerVisible(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START)
+            } else {
+                drawer.openDrawer(GravityCompat.START)
+            }
+        }
+
+
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+
+        navigationView = findViewById(R.id.nav_view)
+
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(navigationView, navController)
+
+
     }
+
+
 }
