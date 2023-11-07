82ee560bbc47eb2f162d8c9f4865852c5e8befa2
frangoldammer <frangoldammer@gmail.com>
2023-11-07 18:24:24 -0300

    funcion subirFoto

--- app/src/main/java/com/example/tp3_grupo7_be/fragments/PerfilFragment.kt
+++ app/src/main/java/com/example/tp3_grupo7_be/fragments/PerfilFragment.kt
@@ -1,19 +1,26 @@
 package com.example.tp3_grupo7_be.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.tp3_grupo7_be.R
-
+import com.google.android.material.imageview.ShapeableImageView
 
 
 class PerfilFragment : Fragment() {
-
-
+    lateinit var btnSubirFoto: Button
+    lateinit var fotoPerfil: ShapeableImageView
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
@@ -21,9 +28,34 @@ class PerfilFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false)
+        val view = inflater.inflate(R.layout.fragment_perfil, container, false)
+        fotoPerfil = view.findViewById(R.id.foto_perfil)
+        btnSubirFoto = view.findViewById(R.id.btn_subir_foto)
+
+        galleryLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+
+                    val data: Intent? = result.data
+                    if (data != null)
+                        fotoPerfil.setImageURI(data.data)// Puedes acceder a los datos de la imagen seleccionada desde 'data'
+                }
+            }
+
+        return view
     }
 
+    override fun onStart() {
+        super.onStart()
 
+        btnSubirFoto.setOnClickListener{
+
+            val iGallery = Intent(Intent.ACTION_PICK)
+            iGallery.type = "image/*"
+            galleryLauncher.launch(iGallery)
+
+        }
+
+
+    }
 }
